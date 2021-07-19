e12ed746f94ba494ac1f7c3a76b986f18677c15e
earthlymeg <meganshulmister@gmail.com>
2021-07-19 12:58:39 -0600

    database modeling

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Song = require('./models/song')
 const app = express();
+
+mongoose.connect('mongodb://localhose/songrecorder', {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
 
 app.set('view engine', 'ejs');
 
+app.use(express.json());
 app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.render('index');
-    // res.send('helloworld')
 })
 
 //TODO
 
-// app.post('/songs', (req, res) => {
-//     req.body()
-// })
+app.post('/songs', async (req, res) => {
+    const song = new Song({
+      notes: req.body.songNotes
+    })
+    await song.save();
+    res.json(song);
+})
 
+app.get('/songs/:id', async (req, res) => {
+   let song;
+   
+   try {
+     song = await Song.findById(req.params.id)
+   } catch (e) {
+     song = undefined;
+   }
+   res.render('index', {song: song})
+   
+   
+})
 
 
 app.listen(4000, () => {
