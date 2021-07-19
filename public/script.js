3728abdf6b70a78bf48bb3733a2caca6da18fa5b
earthlymeg <meganshulmister@gmail.com>
2021-07-19 11:03:23 -0600

    Working on recording logic

--- public/script.js
+++ public/script.js
@@ -1,35 +1,74 @@
-const white_keys = ['z','x','c','v','b','n','m'];
-const black_keys = ['s','d','g','h','j'];
+const white_keys = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const black_keys = ['s', 'd', 'g', 'h', 'j'];
+
+const recordButton = document.querySelector('.record-button')
 const keys = document.querySelectorAll('.key');
 const whiteKeys = document.querySelectorAll('.key.white');
 const blackKeys = document.querySelectorAll('.key.black');
 
-keys.forEach( key => {
+let recordingStartTime; 
+let songNotes;
+
+keys.forEach(key => {
     key.addEventListener('click', () => playNote(key));
 })
 
+
+recordButton.addEventListener('click', toggleRecording)
+
+
 document.addEventListener('keydown', e => {
 
-  if (e.repeat) return;  
-  const key = e.key;
-  const whiteKeyIndex = white_keys.indexOf(key);
-  const blackKeyIndex = black_keys.indexOf(key);
+    if (e.repeat) return;
+    const key = e.key;
+    const whiteKeyIndex = white_keys.indexOf(key);
+    const blackKeyIndex = black_keys.indexOf(key);
 
-  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
-  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
-  
-  
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
+
+
 
 })
- 
+
+function toggleRecording() {
+    recordButton.classList.toggle('active');
+    if (isRecording()) {
+        startRecording();
+    } else {
+        stopRecording();
+    }
+}
+
+function isRecording() {
+    return recordButton != null && recordButton.classList.contains('active');
+}
+
+function startRecording() {
+    //everytime someone plays a note, we will mark what time note is plated + which note
+
+    //get start time
+    recordingStartTime = Date.now()''
+    // record song notes
+    songNotes = [];
+
+}
+
+function stopRecording() {
+    playSong();
+}
+
+function playSong() {
+    console.log(songNoes)
+}
 
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note);
     noteAudio.currentTime = 0;
-    noteAudio.play();    
+    noteAudio.play();
     key.classList.add('active');
     console.log(key)
-   
+
     noteAudio.addEventListener('ended', () => {
         key.classList.remove('active');
         console.log(key)
